efa1d3b87d1bfc838c6d0909c0897a0e4db8d2b2
John Oerter <john@johnoerter.me>
2019-12-28 14:31:24 -0600

    test for X cell state

--- src/app/game-cell/game-cell.component.spec.ts
+++ src/app/game-cell/game-cell.component.spec.ts
@@ -1,3 +1,4 @@
+import { CellState } from '../cell-state.enum';
 import { Spectator, createComponentFactory } from '@ngneat/spectator';
 import { GameCellComponent } from './game-cell.component';
 
@@ -9,5 +10,13 @@ describe('GameCell Component', () => {
 
     it('should be blank when CellState is Blank', () => {
         expect(spectator.query('.cell')).toContainText('');
+    });
+
+    it('should contain an X when CellState is X', () => {
+        const cellState = CellState.X;
+        spectator.setInput('cellState', cellState);
+        spectator.detectChanges();
+
+        expect(spectator.query('.cell')).toContainText('X');
     });
 });
--- src/app/game-cell/game-cell.component.ts
+++ src/app/game-cell/game-cell.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CellState } from '../cell-state.enum';
 
 @Component({
-  selector: 'app-game-cell',
-  templateUrl: './game-cell.component.html',
-  styleUrls: ['./game-cell.component.scss']
+    selector: 'app-game-cell',
+    templateUrl: './game-cell.component.html',
+    styleUrls: ['./game-cell.component.scss']
 })
-export class GameCellComponent implements OnInit {
+export class GameCellComponent {
+    @Input() cellState: CellState;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    xCellState = CellState.X;
 }
